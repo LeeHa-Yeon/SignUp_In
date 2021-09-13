ed207e174eb7e5486178ba199b6d157a8cfcad12
Lee Ha Yeon <gkdus1613@icloud.com>
2021-09-14 04:22:28 +0900

    datePicker을 label에 띄우기

--- SignUp_In/Login/SignUpSecondViewController.swift
+++ SignUp_In/Login/SignUpSecondViewController.swift
@@ -2,8 +2,23 @@ import UIKit
 
 class SignUpSecondViewController: UIViewController {
     
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var setPhone: UITextField!
     @IBOutlet weak var setBirth: UILabel!
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
+    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker){
+        let date: Date = self.datePicker.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.setBirth.text = dateString
+        
+    }
     
     @IBAction func BackButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -12,6 +27,7 @@ class SignUpSecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
     }
 
 }
